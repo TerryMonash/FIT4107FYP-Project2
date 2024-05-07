5baffe38846ba9b6618403def20063e0e4dd822a
LeonMa2001 <leonma271@hotmail.com>
2024-05-08 01:21:08 +1000

    Started on login.js

---
+++ Login.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVWMP8goMC1XDGcs8TcHNQVqIzMcJ_V_c",
+  authDomain: "fit-4170-adaptive-ui.firebaseapp.com",
+  projectId: "fit-4170-adaptive-ui",
+  storageBucket: "fit-4170-adaptive-ui.appspot.com",
+  messagingSenderId: "868861679224",
+  appId: "1:868861679224:web:65f20eb9704822bb98ff8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
